976fa09707f97eb44a1b85d6c86828b98d231b41
sarahcarrr <sarahcarrmarquis@gmail.com>
2017-03-14 20:08:49 -0700

    actually the homework

--- 3_message_app/js/index.js
+++ 3_message_app/js/index.js
@@ -2,22 +2,38 @@ var questions = ["What is your name?", "How's your day?", "What can I help you w
 var answers = [];
 var intCurrentQuestion = 0;
 
+
 $(function(){
 
     askQuestion(intCurrentQuestion);
-
     $("#conversationForm").submit(function(submitEvent){
         submitEvent.preventDefault();
-
         var answer = $("#answer").val();
-        sendAnswer(answer)
+        sendAnswer(answer);
     });
 
 });
+// Step 1 - Write a jQuery on page load anonymous function
+
+    // Step 2 - Call the askQuestion function below and pass it the global intCurrentQuestion variable
+
+    // Step 3 - Select #conversationForm, use the submit event to capture submitEvent with an anonymous function and then .preventDefault()
+        
+        // Declare a variable "answer" and select #answer get the input value
+        
+        // Call sendAnswer() and pass it the local answer variable 
+
 
 function askQuestion(questionNumber)
 {
     console.log("Inside askQuestion:", questionNumber);
+
+    var question = questions[questionNumber];
+    if(question) 
+    {
+        $("#conversation").append('<li class="list-group-item ai">' + questions[questionNumber] + '</li>');
+        intCurrentQuestion = questionNumber;
+    }
     // Declare a variable called "question" and use the global questions array and the local questionNumber to assign the value
     
     // Write an if(question) statement
@@ -31,6 +47,14 @@ function askQuestion(questionNumber)
 function sendAnswer(answer)
 {
     console.log("Inside sendAnswer", answer);
+
+    if(answer)
+    {
+        $("#answer").val('');
+        $("#conversation").append('<li class="list-group-item human">' + answer + '</li>');
+        answers.push(answer);
+        askQuestion(intCurrentQuestion + 1);
+    }
     // Write an if(answer) statement
         
         // Select #answer and set the value to ''
